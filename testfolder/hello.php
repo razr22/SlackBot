97445b024e6bd3da5552282f5eafbdbbe3f89536
Zain Quraishi <zain.quraishi@live.ca>
2018-02-21 22:28:37 -0500

    checking codecov

--- testfolder/hello.php
+++ testfolder/hello.php
@@ -1,4 +1,6 @@
 <?php
+declare(strict_types=1);
+
 	class Student {
 		public $name;
 		public function __construct($name){
--- testfolder/output.php
+++ testfolder/output.php
@@ -1,10 +1,10 @@
 <?php
+declare(strict_types=1);
+
 include_once("hello.php");
 
 	class Output extends \PHPUnit_Framework_TestCase {
-		
 		public function testString(){
-//			$test->expectOutputString('Hello World');
 			$this->student = new Student('Zain Quraishi');
 			$this->assertEquals($this->student->newStudent(), 'Student\'s name is Zain Quraishi' . "\n");
 		}
