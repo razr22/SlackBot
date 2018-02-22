9c8c9c2edcd6eb8157af6bccb09a9e337c608d25
Zain Quraishi <zain.quraishi@live.ca>
2018-02-21 21:18:15 -0500

    added mock unit test

--- testfolder/hello.php
+++ testfolder/hello.php
@@ -1,13 +1,18 @@
-<html>
- <head>
-  <title>PHP Test</title>
- </head>
- <body>
 <?php
-function boo($far)
-{
-  return $far;
-}
+	class Student {
+		public $name;
+		public function __construct($name){
+			$this->name = $name;
+		}
+		public function getName() {
+			return $this->name;
+		}
+	
+		public function newStudent(){
+			return "Student's name is " . $this->name . "\n";
+		}
+	}
+
+	$test = new Student('Zain Quraishi');
+	echo $test->newStudent();
 ?>
- </body>
-</html>
---
+++ testfolder/output.php
@@ -0,0 +1,13 @@
+<?php
+//use PHPUnit\Framework\TestCase;
+include_once("hello.php");
+
+	class Output extends \PHPUnit_Framework_TestCase {
+		
+		public function testString(){
+//			$test->expectOutputString('Hello World');
+			$this->student = new Student('Zain Quraishi');
+			$this->assertEquals($this->student->newStudent(), 'Student\'s name is Zain Quraishi' . "\n");
+		}
+	}
+?>
